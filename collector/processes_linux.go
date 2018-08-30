806ccff6c8dcb64c4635f1cab97b3b2a913509ad
jenkins <jenkins@creditease.cn>
2018-08-30 17:11:58 +0800

    CLOUD-1179: add monitor for single process

--- collector/processes_linux.go
+++ collector/processes_linux.go
@@ -18,24 +18,47 @@ package collector
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/procfs"
 )
 
-type processCollector struct {
-	threadAlloc *prometheus.Desc
-	threadLimit *prometheus.Desc
-	procsState  *prometheus.Desc
-	pidUsed     *prometheus.Desc
-	pidMax      *prometheus.Desc
-}
+type (
+	processCollector struct {
+		threadAlloc               *prometheus.Desc
+		threadLimit               *prometheus.Desc
+		procsState                *prometheus.Desc
+		procsCPUUsage             *prometheus.Desc
+		procsMemoryUsage          *prometheus.Desc
+		procsFileDescriptorOpened *prometheus.Desc
+		pidUsed                   *prometheus.Desc
+		pidMax                    *prometheus.Desc
+	}
+
+	processStatistics struct {
+		threads         int
+		states          map[string]int32
+		cpuUsage        map[string]float64
+		memoryUsage     map[string]float64
+		fileDescriptors map[string]int
+		pids            int
+	}
+)
+
+const (
+	cpuUsageMinThreshold        = 30.0 // min threshold of cpu usage, 30.0 is 30.0%
+	memoryUsageMinThreshold     = 30.0 // min threshold of memory usage, 30.0 is 30.0%
+	fileDescriptorsMinThreshold = 100  // min threshold of number of opened file descriptors, 100 is one hundred opened file descriptors
+)
 
 func init() {
-	registerCollector("processes", defaultDisabled, NewProcessStatCollector)
+	registerCollector("processes", defaultEnabled, NewProcessStatCollector)
 }
 
+// NewProcessStatCollector returns a new Collector exposing all processes statistics.
 func NewProcessStatCollector() (Collector, error) {
 	subsystem := "processes"
 	return &processCollector{
@@ -54,66 +77,159 @@ func NewProcessStatCollector() (Collector, error) {
 			"Number of processes in each state.",
 			[]string{"state"}, nil,
 		),
-		pidUsed: prometheus.NewDesc(prometheus.BuildFQName(namespace, subsystem, "pids"),
-			"Number of PIDs", nil, nil,
+		procsCPUUsage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "cpu_usage"),
+			"CPU usage of a process.",
+			[]string{"command_cpu_usage"}, nil,
 		),
-		pidMax: prometheus.NewDesc(prometheus.BuildFQName(namespace, subsystem, "max_processes"),
-			"Number of max PIDs limit", nil, nil,
+		procsMemoryUsage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "memory_usage"),
+			"Memory usage of a process.",
+			[]string{"command_memory_usage"}, nil,
+		),
+		procsFileDescriptorOpened: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "file_descriptors"),
+			"Total opened file descriptors of a process.",
+			[]string{"command_file_descriptors"}, nil,
+		),
+		pidUsed: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "pids"),
+			"Number of PIDs",
+			nil, nil,
+		),
+		pidMax: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "max_processes"),
+			"Number of max PIDs limit",
+			nil, nil,
 		),
 	}, nil
 }
+
 func (t *processCollector) Update(ch chan<- prometheus.Metric) error {
-	pids, states, threads, err := getAllocatedThreads()
+	stat, err := procfs.NewStat()
+	if err != nil {
+		return fmt.Errorf("unable to retrieve kernel/system statistics read from /proc/stat: %q", err)
+	}
+
+	totalCPUTime := stat.CPUTotal.User + stat.CPUTotal.System
+
+	m := &meminfoCollector{}
+	meminfo, err := m.getMemInfo()
+	if err != nil {
+		return fmt.Errorf("unable to retrieve memory statistics read from /proc/meminfo: %q", err)
+	}
+	totalMemory := meminfo["MemTotal_bytes"]
+
+	processStat, err := getAllocatedThreads(totalCPUTime, totalMemory)
 	if err != nil {
 		return fmt.Errorf("unable to retrieve number of allocated threads: %q", err)
 	}
 
-	ch <- prometheus.MustNewConstMetric(t.threadAlloc, prometheus.GaugeValue, float64(threads))
+	ch <- prometheus.MustNewConstMetric(t.threadAlloc, prometheus.GaugeValue, float64(processStat.threads))
+
 	maxThreads, err := readUintFromFile(procFilePath("sys/kernel/threads-max"))
 	if err != nil {
 		return fmt.Errorf("unable to retrieve limit number of threads: %q", err)
 	}
 	ch <- prometheus.MustNewConstMetric(t.threadLimit, prometheus.GaugeValue, float64(maxThreads))
 
-	for state := range states {
-		ch <- prometheus.MustNewConstMetric(t.procsState, prometheus.GaugeValue, float64(states[state]), state)
+	for state := range processStat.states {
+		ch <- prometheus.MustNewConstMetric(t.procsState, prometheus.GaugeValue, float64(processStat.states[state]), state)
 	}
+
+	for cpuUsage := range processStat.cpuUsage {
+		ch <- prometheus.MustNewConstMetric(t.procsCPUUsage, prometheus.GaugeValue, processStat.cpuUsage[cpuUsage], cpuUsage)
+	}
+
+	for memoryUsage := range processStat.memoryUsage {
+		ch <- prometheus.MustNewConstMetric(t.procsMemoryUsage, prometheus.GaugeValue, processStat.memoryUsage[memoryUsage], memoryUsage)
+	}
+
+	for fd := range processStat.fileDescriptors {
+		ch <- prometheus.MustNewConstMetric(t.procsFileDescriptorOpened, prometheus.GaugeValue, float64(processStat.fileDescriptors[fd]), fd)
+	}
+
+	ch <- prometheus.MustNewConstMetric(t.pidUsed, prometheus.GaugeValue, float64(processStat.pids))
 
 	pidM, err := readUintFromFile(procFilePath("sys/kernel/pid_max"))
 	if err != nil {
 		return fmt.Errorf("unable to retrieve limit number of maximum pids alloved: %q", err)
 	}
-	ch <- prometheus.MustNewConstMetric(t.pidUsed, prometheus.GaugeValue, float64(pids))
 	ch <- prometheus.MustNewConstMetric(t.pidMax, prometheus.GaugeValue, float64(pidM))
 
 	return nil
 }
 
-func getAllocatedThreads() (int, map[string]int32, int, error) {
+func getAllocatedThreads(totalCPUTime, totalMemory float64) (*processStatistics, error) {
 	fs, err := procfs.NewFS(*procPath)
 	if err != nil {
-		return 0, nil, 0, err
+		return nil, err
 	}
-	p, err := fs.AllProcs()
+
+	procs, err := fs.AllProcs()
 	if err != nil {
-		return 0, nil, 0, err
+		return nil, err
 	}
+
 	pids := 0
-	thread := 0
-	procStates := make(map[string]int32)
-	for _, pid := range p {
-		stat, err := pid.NewStat()
+	threads := 0
+	procStates := make(map[string]int32)             // procStates's example: {sleeping: 15, running: 3}
+	procCPUUsage := make(map[string]float64)         // procCPUUsage's example: {java-app: 3.1, golang-app: 9.8}
+	procMemoryUsage := make(map[string]float64)      // procMemoryUsage's example: {java-app: 1.5, golang-app: 5.2}
+	procFileDescriptorOpened := make(map[string]int) // procFileDescriptorOpened's example: {java-app: 3, golang-app: 14}
+	for _, p := range procs {
+		stat, err := p.NewStat()
 		// PIDs can vanish between getting the list and getting stats.
 		if os.IsNotExist(err) {
 			log.Debugf("file not found when retrieving stats: %q", err)
 			continue
 		}
 		if err != nil {
-			return 0, nil, 0, err
+			return nil, err
 		}
+
 		pids += 1
+		threads += stat.NumThreads
 		procStates[stat.State] += 1
-		thread += stat.NumThreads
+
+		cmdLine, err := p.CmdLine()
+		if err != nil {
+			return nil, err
+		}
+
+		cpuTime := stat.CPUTime()
+		cpuTimeUsage, err := strconv.ParseFloat(fmt.Sprintf("%.1f", cpuTime/totalCPUTime*100), 64)
+		if err != nil {
+			return nil, err
+		}
+		if cpuTimeUsage >= cpuUsageMinThreshold {
+			procCPUUsage[strings.Join([]string{strings.Join(cmdLine, "_"), "cpu_usage"}, "_")] = cpuTimeUsage
+		}
+
+		memory := float64(stat.RSS * 4 * 1024)
+		memoryUsage, err := strconv.ParseFloat(fmt.Sprintf("%.1f", memory/totalMemory*100), 64)
+		if err != nil {
+			return nil, err
+		}
+		if memoryUsage >= memoryUsageMinThreshold {
+			procMemoryUsage[strings.Join([]string{strings.Join(cmdLine, "_"), "memory_usage"}, "_")] = memoryUsage
+		}
+
+		fds, err := p.FileDescriptorsLen()
+		if err != nil {
+			return nil, err
+		}
+		if fds >= fileDescriptorsMinThreshold {
+			procFileDescriptorOpened[strings.Join([]string{strings.Join(cmdLine, "_"), "fds"}, "_")] = fds
+		}
 	}
-	return pids, procStates, thread, nil
+
+	return &processStatistics{
+		threads:         threads,
+		states:          procStates,
+		cpuUsage:        procCPUUsage,
+		memoryUsage:     procMemoryUsage,
+		fileDescriptors: procFileDescriptorOpened,
+		pids:            pids,
+	}, nil
 }
--- collector/processes_linux_test.go
+++ collector/processes_linux_test.go
@@ -16,32 +16,63 @@
 package collector
 
 import (
+	"os"
 	"testing"
 
-	"gopkg.in/alecthomas/kingpin.v2"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func TestReadProcessStatus(t *testing.T) {
 	if _, err := kingpin.CommandLine.Parse([]string{"--path.procfs", "fixtures/proc"}); err != nil {
 		t.Fatal(err)
 	}
+
+	file, err := os.Open("fixtures/proc/meminfo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	memInfo, err := parseMemInfo(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	totalMemory := memInfo["MemTotal_bytes"]
+
+	totalCPUTime := float64(301854 + 111922)
+
 	want := 1
-	pids, states, threads, err := getAllocatedThreads()
+	processStat, err := getAllocatedThreads(totalCPUTime, totalMemory)
 	if err != nil {
 		t.Fatalf("Cannot retrieve data from procfs getAllocatedThreads function: %v ", err)
 	}
-	if threads < want {
-		t.Fatalf("Current threads: %d Shouldn't be less than wanted %d", threads, want)
-	}
-	if states == nil {
 
-		t.Fatalf("Process states cannot be nil %v:", states)
+	if processStat.threads < want {
+		t.Fatalf("Current threads: %d Shouldn't be less than wanted %d", processStat.threads, want)
 	}
+
+	if processStat.states == nil {
+		t.Fatalf("Process states cannot be nil %v:", processStat.states)
+	}
+
+	if processStat.cpuUsage == nil {
+		t.Fatalf("Process cpu usage cannot be nil %v:", processStat.cpuUsage)
+	}
+
+	if processStat.memoryUsage == nil {
+		t.Fatalf("Process memory usage cannot be nil %v:", processStat.memoryUsage)
+	}
+
+	if processStat.fileDescriptors == nil {
+		t.Fatalf("Process file descriptors cannot be nil %v:", processStat.fileDescriptors)
+	}
+
 	maxPid, err := readUintFromFile(procFilePath("sys/kernel/pid_max"))
 	if err != nil {
 		t.Fatalf("Unable to retrieve limit number of maximum pids alloved %v\n", err)
 	}
-	if uint64(pids) > maxPid || pids == 0 {
+
+	if uint64(processStat.pids) > maxPid || processStat.pids == 0 {
 		t.Fatalf("Total running pids cannot be greater than %d or equals to 0", maxPid)
 	}
 }
